cf2eb9a86f047f90aee2be9c9d8a9305eb39a532
Syifaaahdr29 <syifaaahdr29@gmail.com>
2024-06-15 19:25:20 +0700

    remove unused files

--- src/main.jsx
+++ src/main.jsx
@@ -7,8 +7,6 @@ import ErrorPage from "./pages/ErrorPage.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import page and components
-import Login from "./pages/login";
-import Register from "./pages/Register";
 import Layout from "./components/Layout.jsx";
 import Landing from "./components/Landing";
 import JenisHamster from "./pages/JenisHamster.jsx";
@@ -25,7 +23,6 @@ const router = createBrowserRouter(
       {/* Ini Layout biar tetep ada di semua halaman. Misalnya Navbar, Footer Bisa di taroh sini */}
       <Route path="/" element={<Layout />} errorElement={<ErrorPage />}>
         <Route path="/" element={<Landing />} />
-        <Route path="/" element={<Landing />} />
         {/* Di bawah sini bisa di taroh tiap halaman yang dibuat Contohnya :  */}
         {/* <Route path="/home" element={<Home />} /> */}
         <Route path="/jenishamster" element={<JenisHamster />}/>
@@ -36,9 +33,6 @@ const router = createBrowserRouter(
         <Route path="/forum" element={<Forum />}/>
         {/* <Route path="/forum" element={<Forum />}/> */}
       </Route>
-
-      <Route path="/login" element={<Login />} />
-      <Route path="/regis" element={<Register />} />
     </>
   )
 );
